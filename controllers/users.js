4d6d4c4fd6fdfe1c2cbd17c5490d875d152df253
Pasha Litvak <parsifalty@gmail.com>
2023-11-07 22:11:44 +0200

    fosd

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,23 @@
-const User = require("../models/user");
-const NotFoundError = require("../errors/NotFoundError");
-const CastError = require("../errors/CastError");
-const ValidationError = require("../errors/ValidationError");
+const User = require('../models/user');
+const NotFoundError = require('../errors/NotFoundError');
+const ValidationError = require('../errors/ValidationError');
 
 module.exports.getUser = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() =>
-      res.status(500).send({ message: "На сервере произошла ошибка" })
-    );
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        throw new NotFoundError("Пользователь с этим айди не найден");
+        throw new NotFoundError('Пользователь с этим айди не найден');
       } else {
         res.send(user);
       }
     })
-    .catch(() =>
-      res.status(500).send({ message: "На сервере произошла ошибка" })
-    );
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.addUser = (req, res) => {
@@ -31,9 +26,9 @@ module.exports.addUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send({ data: user }))
     .catch((err) => {
-      if (err.name === "ValidationError") {
+      if (err.name === 'ValidationError') {
         throw new ValidationError(
-          "Переданы некорректные данные при создании пользователя"
+          'Переданы некорректные данные при создании пользователя',
         );
       }
     });
@@ -45,18 +40,18 @@ module.exports.editUserData = (req, res) => {
     User.findByIdAndUpdate(
       req.user._id,
       { name, about },
-      { new: "true", runValidators: true }
+      { new: 'true', runValidators: true },
     )
       .then((user) => res.send(user))
       .catch((err) => {
-        if (err.name === "ValidationError") {
+        if (err.name === 'ValidationError') {
           throw new ValidationError(err.message);
         } else {
-          throw new NotFoundError("Пользователь с этим айди не был найден");
+          throw new NotFoundError('Пользователь с этим айди не был найден');
         }
       });
   } else {
-    res.status(500).send({ message: "На сервере произошла ошибка" });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 };
 
@@ -65,17 +60,17 @@ module.exports.editUserAvatar = (req, res) => {
     User.findByIdAndUpdate(
       req.user._id,
       { avatar: req.body.avatar },
-      { new: "true", runValidators: true }
+      { new: 'true', runValidators: true },
     )
       .then((user) => res.status(200).send(user))
       .catch((err) => {
-        if (err.name === "ValidationError") {
+        if (err.name === 'ValidationError') {
           throw new ValidationError(err.message);
         } else {
-          throw new NotFoundError("Пользователь с этим айди не был найден");
+          throw new NotFoundError('Пользователь с этим айди не был найден');
         }
       });
   } else {
-    res.status(500).send({ message: "На сервере произошла ошибка" });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 };
