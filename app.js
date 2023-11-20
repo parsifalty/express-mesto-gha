b144020d520d18f4193613321547039fbef3c98e
Pasha Litvak <parsifalty@gmail.com>
2023-11-20 02:28:48 +0200

    opsdfpoko

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { NOT_FOUND_STATUS } = require('./constants');
-const auth = require('./middlewares/auth');
-const { login, addUser } = require('./controllers/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const { NOT_FOUND_STATUS } = require("./constants");
+const auth = require("./middlewares/auth");
+const { login, addUser } = require("./controllers/users");
 
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = "mongodb://localhost:27017/mestodb" } =
+  process.env;
 const app = express();
 
 app.use(express.json());
@@ -15,16 +16,16 @@ mongoose.connect(DB_URL, {
   useUnifiedTopology: true,
 });
 
-app.use('/singup', addUser);
-app.use('/singin', login);
+app.post("/singup", addUser);
+app.post("/singin", login);
 
 app.use(auth);
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+app.use("/users", require("./routes/users"));
+app.use("/cards", require("./routes/cards"));
 
-app.use('*', (req, res) => {
-  res.status(NOT_FOUND_STATUS).send({ message: 'Данная страница не найдена' });
+app.use("*", (req, res) => {
+  res.status(NOT_FOUND_STATUS).send({ message: "Данная страница не найдена" });
 });
 
 app.listen(PORT, () => {});
